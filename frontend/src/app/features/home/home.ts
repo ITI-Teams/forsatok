12057328686b8803c5a5a248de36580b39c5bed8
Abdallah EL-SAIED <abdallahalsabaa.pu.2021@gmail.com>
2025-11-06 11:15:00 +0200

    Create How it works? Section

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../home-parts/hero/hero';
+import { HowItWork } from "../home-parts/how-it-work/how-it-work";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero],
+  imports: [Hero, HowItWork],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
