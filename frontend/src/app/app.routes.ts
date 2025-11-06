70d35a88b23b7f7c958bc907ad09ec64a20a7852
HagarElhalfawy <hagarelhalfawy@gmail.com>
2025-11-06 22:17:25 +0200

    Create Candidates component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -15,6 +15,7 @@ import { NotFound } from './features/not-found/not-found';
 import { Employers } from './features/employers/employers';
 import { ContactUs } from './features/contact-us/contact-us';
 import { CandidateProfile } from './features/candidate-profile/candidate-profile';
+import { Candidates } from './features/candidates/candidates';
 
 // ============ Routes ============
 export const routes: Routes = [
@@ -30,7 +31,7 @@ export const routes: Routes = [
       // { path: 'blog', component: Blog },
       // { path: 'contact', component: Contact },
       { path: 'employers', component: Employers },
-      // { path: 'candidates', component: Candidates },
+      { path: 'candidates', component: Candidates },
       // {
       // path: 'jobs',
       // component: Jobs
