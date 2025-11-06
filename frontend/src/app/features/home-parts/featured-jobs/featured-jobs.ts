d02702534a8eecc8a443e6bac83cef919fa2d9fd
Abdallah EL-SAIED <abdallahalsabaa.pu.2021@gmail.com>
2025-11-06 10:26:58 +0200

    feat: - edit layout component - create home page structure - create & complete hero section - update .getignore file to ignore videos

---
+++ frontend/src/app/features/home-parts/featured-jobs/featured-jobs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-jobs',
+  imports: [],
+  templateUrl: './featured-jobs.html',
+  styleUrl: './featured-jobs.css',
+})
+export class FeaturedJobs {
+
+}
