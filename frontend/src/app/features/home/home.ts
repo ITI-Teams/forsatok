8a7460cc08fac0c6dded357bff4cf1551f0df9ff
Abdallah EL-SAIED <abdallahalsabaa.pu.2021@gmail.com>
2025-11-07 13:54:26 +0200

    Create Featured Cities Section & Update Header & Hero Section

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Hero } from '../home-parts/hero/hero';
 import { HowItWork } from "../home-parts/how-it-work/how-it-work";
 import { FeaturedJobs } from "../home-parts/featured-jobs/featured-jobs";
+import { FeaturedCities } from "../home-parts/featured-cities/featured-cities";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, HowItWork, FeaturedJobs],
+  imports: [Hero, HowItWork, FeaturedJobs, FeaturedCities],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
