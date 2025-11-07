b0e405b93376df71a031334b89dc36367a7b8b5e
Abdallah EL-SAIED <abdallahalsabaa.pu.2021@gmail.com>
2025-11-07 14:23:02 +0200

    Create Your Dream Job Section

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -3,11 +3,12 @@ import { Hero } from '../home-parts/hero/hero';
 import { HowItWork } from "../home-parts/how-it-work/how-it-work";
 import { FeaturedJobs } from "../home-parts/featured-jobs/featured-jobs";
 import { FeaturedCities } from "../home-parts/featured-cities/featured-cities";
+import { FindYourDreamJob } from "../home-parts/find-your-dream-job/find-your-dream-job";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, HowItWork, FeaturedJobs, FeaturedCities],
+  imports: [Hero, HowItWork, FeaturedJobs, FeaturedCities, FindYourDreamJob],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
