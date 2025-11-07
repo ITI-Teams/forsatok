ade72a83d219e02c04332368da9a2d37ccc60c5a
Abdallah EL-SAIED <abdallahalsabaa.pu.2021@gmail.com>
2025-11-07 15:30:29 +0200

    Create Featured Candidates Section

--- frontend/src/app/features/home-parts/featured-candidates/featured-candidates.ts
+++ frontend/src/app/features/home-parts/featured-candidates/featured-candidates.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-featured-candidates',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './featured-candidates.html',
-  styleUrl: './featured-candidates.css',
+  styleUrls: ['./featured-candidates.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FeaturedCandidates {
+  candidates = [
+    { id: 1, name: 'Darlene Robertson', title: 'UI Designer', location: 'London, UK', image: '' },
+    { id: 2, name: 'Floyd Miles', title: 'Chartered Accountant', location: 'London, UK', image: '' },
+    { id: 3, name: 'Wade Warren', title: 'Developer', location: 'London, UK', image: '' },
+    { id: 4, name: 'Floyd Miles', title: 'Chartered Accountant', location: 'London, UK', image: '' },
+    { id: 5, name: 'Leslie Alexander', title: 'Marketing Expert', location: 'London, UK', image: '' },
+    { id: 6, name: 'Floyd Miles', title: 'Chartered Accountant', location: 'London, UK', image: '' },
+    { id: 8, name: 'Darlene Robertson', title: 'UI Designer', location: 'London, UK', image: '' },
+    { id: 9, name: 'Wade Warren', title: 'Developer', location: 'London, UK', image: '' },
+  ];
 
+  ngOnInit() {
+    import('swiper/element/bundle').then(({ register }) => register());
+  }
 }
--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -4,11 +4,12 @@ import { HowItWork } from "../home-parts/how-it-work/how-it-work";
 import { FeaturedJobs } from "../home-parts/featured-jobs/featured-jobs";
 import { FeaturedCities } from "../home-parts/featured-cities/featured-cities";
 import { FindYourDreamJob } from "../home-parts/find-your-dream-job/find-your-dream-job";
+import { FeaturedCandidates } from "../home-parts/featured-candidates/featured-candidates";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, HowItWork, FeaturedJobs, FeaturedCities, FindYourDreamJob],
+  imports: [Hero, HowItWork, FeaturedJobs, FeaturedCities, FindYourDreamJob, FeaturedCandidates],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
