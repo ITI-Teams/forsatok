f473a0b96e03560505219f96a96801ea75b6a564
AbdullahShokr1 <darkthe254@gmail.com>
2025-11-14 02:36:00 +0200

    Fix Home API

--- backend/app/Domains/Home/Controllers/Api/HomeController.php
+++ backend/app/Domains/Home/Controllers/Api/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
 
         $jobs = JobPost::select([
                 'id', 'title', 'experience', 'description',
-                'salary_min', 'salary_max', 'type', 'deadline', 'is_active'
+                'salary_min', 'salary_max', 'work_type', 'deadline', 'is_active'
             ])
             ->where('is_active', true)
             ->latest()
